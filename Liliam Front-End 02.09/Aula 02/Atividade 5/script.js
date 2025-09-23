a06d20fc77cd55b7e13b646d933048058cce5146
Liliamapolo <liliam.a.oliveira@gmail.com>
2025-09-23 14:15:37 -0300

    Delete Liliam Front-End 02.09 directory

--- Liliam Front-End 02.09/Aula 02/Atividade 5/script.js
+++
@@ -1,9 +0,0 @@
-function apresentar() {
-  let nome = "Liliam";
-  let hobby = "treinar boxe e ir à praia";
-
-  const paragrafo = document.getElementById("mensagem");
-
-  paragrafo.textContent =
-    "Olá, eu sou " + nome + " e gosto muito de " + hobby + "!";
-}
